c08da704805b435235ae904f32cf6c01c00d3e8b
toanld <le.toan@horusoftaceae.com>
2026-01-10 12:25:53 +0700

    feat: add model classes for Admin, Course, and Student with basic attributes and methods

---
+++ src/main/java/com/session2/model/Course.java
@@ -0,0 +1,50 @@
+package com.session2.model;
+
+public class Course {
+    private Integer id;
+    private String name;
+    private Integer duration;
+    private String instructor;
+    private Date createAt;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDuration() {
+        return this.duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getInstructor() {
+        return this.instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public Date getCreateAt() {
+        return this.createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+}
